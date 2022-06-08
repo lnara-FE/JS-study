bf10c01e033112a5facbd6105d2657655afb50c1
lnara-FE <naas0974@gmail.com>
2022-06-09 01:48:58 +0900

    feat : 메뉴 삭제 기능 구현

--- moonbucks-menu/src/js/index.js
+++ moonbucks-menu/src/js/index.js
@@ -6,18 +6,34 @@
 // - [O] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [O] 메뉴가 추가되고 나면, input은 빈 값으로 초기화된다.
 // - [O] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// TODO 메뉴 수정
+// - [O] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+// - [O] 모달창에서 신규 메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
 
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  // TODO 메뉴 수정
-  // - [O] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
-  // - [O] 모달창에서 신규 메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
+  // TODO 메뉴 삭제
+  // - [O] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창(confirm)이 뜬다.
+  // - [O] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+  // - [O] 총 메뉴 갯수를 count하여 상단에 보여준다.
+  const updateMenuCount = () => {
+    const menuCount = $('#menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerText = `총 ${menuCount} 개`;
+  }
+
   $('#menu-list').addEventListener('click', (e) => {
     if (e.target.classList.contains('menu-edit-button')) {
-      const $menuName = e.target.closest('li').querySelector('.menu-name')
+      const $menuName = e.target.closest('li').querySelector('.menu-name');
       const updatedMenuName = prompt('메뉴명을 수정하세요.', $menuName.innerText);
       $menuName.innerText = updatedMenuName;
+    }
+
+    if (e.target.classList.contains('menu-remove-button')) {
+      if (confirm('정말 삭제하시겠습니다?')) {
+        e.target.closest('li').remove();
+        updateMenuCount();
+      }
     }
   });
 
@@ -51,9 +67,7 @@ function App() {
         </li>`;
     }
     $('#menu-list').insertAdjacentHTML('beforeend', menuItemTemplate(menuName));
-
-    const menuCount = $('#menu-list').querySelectorAll('li').length;
-    $('.menu-count').innerText = `총 ${menuCount} 개`;
+    updateMenuCount();
     $('#menu-name').value = '';
   }
 
